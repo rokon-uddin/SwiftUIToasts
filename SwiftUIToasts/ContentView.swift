a3d17723efe236689bd1cf19a38c1f9cc5c6de04
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-10-23 11:11:57 +0600

    Initial Commit

---
+++ SwiftUIToasts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIToasts
+//
+//  Created by Mohammed Rokon Uddin on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIToasts/SwiftUIToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToastsApp.swift
+//  SwiftUIToasts
+//
+//  Created by Mohammed Rokon Uddin on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
